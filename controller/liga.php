cc427771504434bbf61414e996ac8892d5041bb6
Tomas Alvarez <l.aalt011003@itses.edu.mx>
2022-10-07 18:51:01 -0500

    Problemas en los servicios

--- controller/liga.php
+++ controller/liga.php
@@ -7,31 +7,62 @@ $body = json_decode(file_get_contents("php://input"), true);
 
 switch ($_GET["opcion"]) {
 
+// Partidos
+
     case "GetAllPartido":
         $datos = $liga->get_partidos();
         echo json_encode($datos);
         break;
 
-    // case "GetId":
-    //     $datos = $liga->get_heroe_x_id($body["heroe_id"]);
-    //     echo json_encode($datos);
-    //     break;
+    case "GetIdPartido":
+        $datos = $liga->get_partido_x_id($body["partido_id"]);
+        echo json_encode($datos);
+        break;
 
     case "InsertPartido":
         $datos = $liga->insert_partido($body["arbitro"], $body["local"], $body["visita"]);
         echo json_encode($datos);
         break;
 
-    // case "Update":
-    //     $datos = $liga->update_heroe($body["id"], $body["nombre"], $body["imagen"], $body["descripcion"], $body["categoria"], $body["genero"]);
-    //     echo json_encode($datos);
-    //     break;
+    case "UpdatePartido":
+        $datos = $liga->update_partido($body["id"], $body["arbitro"], $body["local"], $body["visita"]);
+        echo json_encode($datos);
+        break;
 
-    // case "Delete":
-    //     $datos = $liga->delete_heroe($body["id"]);
-    //     echo json_encode($datos);
-    //     break;
+    case "DeletePartido":
+        $datos = $liga->delete_partido($body["id"]);
+        echo json_encode($datos);
+        break;
 
+// ------------------------------------------------------ Equipo
+
+    case "GetAllEquipo":
+        $datos = $liga->get_equipos();
+        echo json_encode($datos);
+        break;
+
+    case "GetIdEquipo":
+        $datos = $liga->get_equipo_x_id($body["equipo_id"]);
+        echo json_encode($datos);
+        break;
+
+    case "InsertEquipo":
+        $datos = $liga->insert_equipo($body["nombre"], $body["jugador"]);
+        echo json_encode($datos);
+        break;
+
+    case "UpdateEquipo":
+        $datos = $liga->update_equipo($body["id"], $body["nombre"], $body["jugador"]);
+        echo json_encode($datos);
+        break;
+
+    case "DeleteEquipo":
+        $datos = $liga->delete_equipo($body["id"]);
+        echo json_encode($datos);
+        break;
+
+
+// --------------------------------------------------------------- Login
     case "Login":
         $datos = $liga->login($body["user"], $body["password"]);
         echo json_encode($datos);
--- models/Liga.php
+++ models/Liga.php
@@ -19,6 +19,8 @@ class Liga extends Conectar
         return $Array;
     }
 
+// Partidos
+
     public function get_partidos()
     {
         $db = parent::conexion();
@@ -37,23 +39,21 @@ class Liga extends Conectar
         return $Array;
     }
 
-//     public function get_heroe_x_id($heroe_id)
-//     {
-//         $conectar = parent::conexion();
-//         parent::set_names();
-//         $sql = "SELECT * FROM `heroes` WHERE id = ?;";
-//         $sql = $conectar->prepare($sql);
-//         $sql->bindValue(1, $heroe_id);
-//         $sql->execute();
-//         $resultado = $sql->fetch(PDO::FETCH_OBJ);
-//         $Array = $resultado ? [
-//             'id' => (int)$resultado->id, 'nombre' => $resultado->nombre,
-//             'imagen' => $resultado->imagen, 'descripcion' => $resultado->descripcion,
-//             'categoria' => $resultado->categoria, 'genero' => $resultado->genero,
-//             'fecha' => $resultado->fecha
-//         ] : [];
-//         return $Array;
-//     }
+    public function get_partido_x_id($partido_id)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "SELECT * FROM `vistapartido` WHERE id = ?;";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $partido_id);
+        $sql->execute();
+        $resultado = $sql->fetch(PDO::FETCH_OBJ);
+        $Array = $resultado ? [
+            'partido' => (int)$resultado->id, 'equipolocal' => $resultado->local, 'equipovisitante' => $resultado->visita,
+                'arbitro' => $resultado->arbitro
+        ] : [];
+        return $Array;
+    }
 
     public function insert_partido($arbitro, $local, $visita)
     {
@@ -72,30 +72,102 @@ class Liga extends Conectar
         return $resultado;
     }
 
-//     public function update_heroe($id, $nombre, $imagen, $descripcion, $categoria, $genero)
-//     {
-//         $conectar = parent::conexion();
-//         parent::set_names();
-//         $sql = "UPDATE `heroes` SET `nombre`= ?, `imagen`= ?, `descripcion`= ?, `categoria`= ?, `genero`= ? WHERE id = ?;";
-//         $sql = $conectar->prepare($sql);
-//         $sql->bindValue(1, $nombre);
-//         $sql->bindValue(2, $imagen);
-//         $sql->bindValue(3, $descripcion);
-//         $sql->bindValue(4, $categoria);
-//         $sql->bindValue(5, $genero);
-//         $sql->bindValue(6, $id);
-//         $resultado['estatus'] = $sql->execute();
-//         return $resultado;
-//     }
+    public function update_partido($id,$arbitro, $local, $visita)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "UPDATE `vistapartido` SET `arbitro`= ?, `equipolocal`= ?, `equipovisitante`= ? WHERE id = ?;";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $arbitro);
+        $sql->bindValue(2, $local);
+        $sql->bindValue(3, $visita);
+        $sql->bindValue(4, $id);
+        $resultado['estatus'] = $sql->execute();
+        return $resultado;
+    }
 
-//     public function delete_heroe($id)
-//     {
-//         $conectar = parent::conexion();
-//         parent::set_names();
-//         $sql = "DELETE FROM `heroes` WHERE id = ?;";
-//         $sql = $conectar->prepare($sql);
-//         $sql->bindValue(1, $id);
-//         $resultado['estatus'] = $sql->execute();
-//         return $resultado;
-//     }
+    public function delete_partido($id)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "DELETE FROM `vistapartido` WHERE id = ?;";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $id);
+        $resultado['estatus'] = $sql->execute();
+        return $resultado;
+    }
+
+// -------------------------------------------------------------------Equipo
+
+    public function get_equipos()
+    {
+        $db = parent::conexion();
+        parent::set_names();
+        $sql = "SELECT * FROM `vistaequipo`;";
+        $sql = $db->prepare($sql);
+        $sql->execute();
+        $resultado = $sql->fetchAll(PDO::FETCH_OBJ);
+        $Array = [];
+        foreach ($resultado as $d) {
+            $Array[] = [
+                'equipo' => (int)$d->id, 'nombre' => $d->nombre, 'jugador' => $d->jugador
+            ];
+        }
+        return $Array;
+    }
+
+    public function get_equipo_x_id($equipo_id)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "SELECT * FROM `vistaequipo` WHERE id = ?;";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $equipo_id);
+        $sql->execute();
+        $resultado = $sql->fetch(PDO::FETCH_OBJ);
+        $Array = $resultado ? [
+            'equipo' => (int)$resultado->id, 'nombre' => $resultado->nombre, 'jugador' => $resultado->jugador
+        ] : [];
+        return $Array;
+    }
+
+    public function insert_equipo($nombre, $jugador)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "INSERT INTO `vistaequipo`(`nombre`, `jugador`) VALUES (?,?);";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $nombre);
+        $sql->bindValue(2, $jugador);
+        $resultado['estatus'] =  $sql->execute();
+        $lastInserId =  $conectar->lastInsertId();
+        if ($lastInserId != "0") {
+            $resultado['id'] = (int)$lastInserId;
+        }
+        return $resultado;
+    }
+
+    public function update_equipo($id,$nombre, $jugador)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "UPDATE `vistaequipo` SET `nombre`= ?, `jugador`= ? WHERE id = ?;";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $nombre);
+        $sql->bindValue(2, $jugador);
+        $sql->bindValue(3, $id);
+        $resultado['estatus'] = $sql->execute();
+        return $resultado;
+    }
+
+    public function delete_equipo($id)
+    {
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql = "DELETE FROM `vistaequipo` WHERE id = ?;";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $id);
+        $resultado['estatus'] = $sql->execute();
+        return $resultado;
+    }
 }
